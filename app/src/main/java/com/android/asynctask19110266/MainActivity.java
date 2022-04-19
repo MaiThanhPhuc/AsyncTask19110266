bbdfb2689011e2940952c2f6c0ec814cfb6d7969
MaiThanhPhuc <maithanhphuc7721@gmail.com>
2022-04-19 14:14:10 +0700

    Initial commit

---
+++ app/src/main/java/com/android/asynctask19110266/MainActivity.java
@@ -0,0 +1,84 @@
+package com.android.asynctask19110266;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.txtStatus);
+    }
+    public void startAsyncTask(View v) {
+        ExampleAsyncTask task = new ExampleAsyncTask(this);
+        textView.setText("Working...");
+        task.execute();
+    }
+
+    private static class ExampleAsyncTask extends AsyncTask<Integer, Integer, String> {
+        private WeakReference<MainActivity> activityWeakReference;
+
+        ExampleAsyncTask(MainActivity activity) {
+            activityWeakReference = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            activity.progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            for (int i = 1; i <= 60; i++) {
+                int number = (i * 100 / 60);
+                publishProgress(number);
+                SystemClock.sleep(1000);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+
+            activity.progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            MainActivity activity = activityWeakReference.get();
+            if (activity == null || activity.isFinishing()) {
+                return;
+            }
+            activity.progressBar.setProgress(0);
+            activity.textView.setText("Napping...");
+        }
+    }
+}
